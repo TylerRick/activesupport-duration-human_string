27f887dacec533ada3f425cec903355f46ff908a
Tyler Rick <tyler@tylerrick.com>
2019-04-02 10:36:32 -0700

    Initial commit

---
+++ lib/activesupport/duration/human_string.rb
@@ -0,0 +1,10 @@
+require "activesupport/duration/human_string/version"
+
+module Activesupport
+  module Duration
+    module HumanString
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/activesupport/duration/human_string/version.rb
@@ -0,0 +1,7 @@
+module Activesupport
+  module Duration
+    module HumanString
+      VERSION = "0.1.0"
+    end
+  end
+end
